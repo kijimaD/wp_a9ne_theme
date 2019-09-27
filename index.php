cafec2ffcb0b8c489ddfc450be40bf92c5e8aa4e
kijima <norimaking777@gmail.com>
2019-09-28 04:21:47 +0900

    テーマファイル全損。再作成… 最低限必要なものを追加した。

---
+++ index.php
@@ -0,0 +1,240 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link href="http://localhost/wordpress_bs_test/wp-content/themes/newa9ne_blog_theme/style.css" rel="stylesheet" />
+
+  <!-- 追加CSS -->
+  <link href="http://localhost/wordpress_bs_test/wp-content/themes/newa9ne_blog_theme/css/custom.css" type="text/css" rel="stylesheet" />
+
+  <!-- fontawesome CDN -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+
+  <title>
+    X COFFEE  </title>
+  <meta name='robots' content='noindex,follow' />
+<link rel='dns-prefetch' href='//s.w.org' />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/12.0.0-1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/12.0.0-1\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/localhost\/wordpress_bs_test\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.2.3"}};
+			!function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55357,56424,55356,57342,8205,55358,56605,8205,55357,56424,55356,57340],[55357,56424,55356,57342,8203,55358,56605,8203,55357,56424,55356,57340]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+	<link rel='stylesheet' id='dashicons-css'  href='http://localhost/wordpress_bs_test/wp-includes/css/dashicons.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='admin-bar-css'  href='http://localhost/wordpress_bs_test/wp-includes/css/admin-bar.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='wp-block-library-css'  href='http://localhost/wordpress_bs_test/wp-includes/css/dist/block-library/style.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='contact-form-7-css'  href='http://localhost/wordpress_bs_test/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=5.1.4' type='text/css' media='all' />
+<script type='text/javascript' src='http://localhost/wordpress_bs_test/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script type='text/javascript' src='http://localhost/wordpress_bs_test/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript' src='http://localhost/wordpress_bs_test/wp-content/themes/newa9ne_blog_theme/js/bootstrap.bundle.js?ver=5.2.3'></script>
+<link rel='https://api.w.org/' href='http://localhost/wordpress_bs_test/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://localhost/wordpress_bs_test/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://localhost/wordpress_bs_test/wp-includes/wlwmanifest.xml" />
+<meta name="generator" content="WordPress 5.2.3" />
+<link rel="canonical" href="http://localhost/wordpress_bs_test/" />
+<link rel='shortlink' href='http://localhost/wordpress_bs_test/' />
+<link rel="alternate" type="application/json+oembed" href="http://localhost/wordpress_bs_test/wp-json/oembed/1.0/embed?url=http%3A%2F%2Flocalhost%2Fwordpress_bs_test%2F" />
+<link rel="alternate" type="text/xml+oembed" href="http://localhost/wordpress_bs_test/wp-json/oembed/1.0/embed?url=http%3A%2F%2Flocalhost%2Fwordpress_bs_test%2F&#038;format=xml" />
+<style type="text/css" media="print">#wpadminbar { display:none; }</style>
+	<style type="text/css" media="screen">
+	html { margin-top: 32px !important; }
+	* html body { margin-top: 32px !important; }
+	@media screen and ( max-width: 782px ) {
+		html { margin-top: 46px !important; }
+		* html body { margin-top: 46px !important; }
+	}
+</style>
+	</head>
+
+<!-- ヘッダー -->
+<header class="">
+  <!-- /ヘッダー -->
+
+  <!-- ナビゲーションバー -->
+  <nav class="navbar navbar-expand-md navbar-light sticky-top">
+    <!-- サブコンポーネント -->
+    <div class="container">
+      <!-- ブランド -->
+      <a class="navbar-brand" href="http://localhost/wordpress_bs_test/" style="font-weight:900;">
+        X COFFEE</a>
+      <!-- 切り替えボタン -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!--  ナビゲーション -->
+      <div class="collapse navbar-collapse" id="navbar-content">
+        <!-- ナビゲーションメニュー -->
+        <!-- 左側メニュー: トップページの各コンテンツへのリンク -->
+        <ul class="navbar-nav mr-auto">
+
+        </ul>
+
+        <!-- 右側メニュー: Contactページへのリンク -->
+        <ul class="navbar-nav py-3">
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="http://localhost/wordpress_bs_test//web/">WEB制作</a>
+          </li>
+          <li class="nav-item mx-2">
+            <a class="nav-link" href="http://localhost/wordpress_bs_test//question/">お問い合わせ</a>
+          </li>
+          <!-- ドロップダウン -->
+          <li class="nav-item dropdown mx-2">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              店舗情報
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="http://localhost/wordpress_bs_test/#shop">情報</a>
+              <a class="dropdown-item" href="http://localhost/wordpress_bs_test/#access">アクセス</a>
+            </div>
+          </li>
+          <!-- 検索バー -->
+          <li class="nav-item">
+            <button type="button" class="btn btn-outline-secondary border-0" data-toggle="modal" data-target="#modal01">
+              <i class="fas fa-search"></i>
+            </button>
+          </li>
+
+          <!-- /検索バー -->
+        </ul>
+        <!-- /ナビゲーションメニュー -->
+      </div>
+    </div>
+    <!-- /サブコンポーネント -->
+  </nav>
+  <!-- /ナビゲーションバー -->
+</header>
+
+<!-- 検索モーダル内 -->
+<div class="modal fade" id="modal01" tabindex="-1" role="dialog" aria-labelledby="modal01-label" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modal01-label">サイト内検索</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form class="form-inline" action="http://localhost/wordpress_bs_test/" method="get">
+          <input class="form-control" type="text" placeholder="検索" aria-label="Search" name="s">
+          <button class="btn btn-outline-success" type=submit>
+            <i class="fas fa-search"></i>
+          </button>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- 検索モーダル内 -->
+
+<body>
+  <div class="container my-5">
+    <div class="row">
+      <!-- 未定 -->
+      <div class="col-lg-2">
+      </div>
+      <!-- /未定 -->
+
+      <!-- 本文 -->
+      <div class="col-lg-7">
+        <div class="card border-0">
+          <div class="card-body">
+            <h2 class="card-title" style="font-weight:900">タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</h2>
+            <small class="card-text btn btn-light btn-sm">タグ1</small>
+            <small class="card-text btn btn-light btn-sm">タグ2</small>
+            <small class="card-text">2019/08/26</small>
+            <p class="card-text my-3">これは抜粋ですこれは抜粋ですこれは抜粋ですこれは抜粋ですこれは抜粋ですこれは抜粋ですこれは抜粋ですこれは抜粋です...</p>
+            <a href="#" class="btn btn-outline-success">本文を読む</a>
+          </div>
+        </div>
+        <!-- 繰り返し -->
+        <div class="card-body">
+          <h2 class="card-title" style="font-weight:900">タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</h2>
+          <small class="card-text btn btn-light btn-sm">タグ1</small>
+          <small class="card-text btn btn-light btn-sm">タグ2</small>
+          <small class="card-text">2019/08/26</small>
+          <p class="card-text my-3">これは抜粋ですこれは抜粋ですこれは抜粋ですこれは抜粋ですこれは抜粋ですこれは抜粋ですこれは抜粋ですこれは抜粋です...</p>
+          <a href="#" class="btn btn-outline-success">本文を読む</a>
+        </div>
+        <!-- /繰り返し -->
+
+        <!-- ペジネーション -->
+<nav class="mx-auto">
+  </nav>
+<!-- /ペジネーション -->
+
+      </div>
+      <!-- /本文 -->
+
+      <!-- 未定 -->
+<div class="col-lg-3">
+</div>
+<!-- /未定 -->
+
+</div>
+</div>
+</body>
+      <footer class="py-4">
+  <div class="container text-center">
+    <hr>
+    <p><small>Copyright &copy;
+        2019        X COFFEE, All Rights Reserved.</small></p>
+  </div>
+</footer>
+<script type='text/javascript' src='http://localhost/wordpress_bs_test/wp-includes/js/admin-bar.min.js?ver=5.2.3'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var wpcf7 = {"apiSettings":{"root":"http:\/\/localhost\/wordpress_bs_test\/wp-json\/contact-form-7\/v1","namespace":"contact-form-7\/v1"}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://localhost/wordpress_bs_test/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=5.1.4'></script>
+<script type='text/javascript' src='http://localhost/wordpress_bs_test/wp-includes/js/wp-embed.min.js?ver=5.2.3'></script>
+	<!--[if lte IE 8]>
+		<script type="text/javascript">
+			document.body.className = document.body.className.replace( /(^|\s)(no-)?customize-support(?=\s|$)/, '' ) + ' no-customize-support';
+		</script>
+	<![endif]-->
+	<!--[if gte IE 9]><!-->
+		<script type="text/javascript">
+			(function() {
+				var request, b = document.body, c = 'className', cs = 'customize-support', rcs = new RegExp('(^|\\s+)(no-)?'+cs+'(\\s+|$)');
+
+						request = true;
+
+				b[c] = b[c].replace( rcs, ' ' );
+				// The customizer requires postMessage and CORS (if the site is cross domain)
+				b[c] += ( window.postMessage && request ? ' ' : ' no-' ) + cs;
+			}());
+		</script>
+	<!--<![endif]-->
+			<div id="wpadminbar" class="nojq nojs">
+							<a class="screen-reader-shortcut" href="#wp-toolbar" tabindex="1">ツールバーへスキップ</a>
+						<div class="quicklinks" id="wp-toolbar" role="navigation" aria-label="ツールバー">
+				<ul id='wp-admin-bar-root-default' class="ab-top-menu"><li id='wp-admin-bar-wp-logo' class="menupop"><a class='ab-item' aria-haspopup="true" href='http://localhost/wordpress_bs_test/wp-admin/about.php'><span class="ab-icon"></span><span class="screen-reader-text">WordPress について</span></a><div class="ab-sub-wrapper"><ul id='wp-admin-bar-wp-logo-default' class="ab-submenu"><li id='wp-admin-bar-about'><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-admin/about.php'>WordPress について</a></li></ul><ul id='wp-admin-bar-wp-logo-external' class="ab-sub-secondary ab-submenu"><li id='wp-admin-bar-wporg'><a class='ab-item' href='https://ja.wordpress.org/'>WordPress.org</a></li><li id='wp-admin-bar-documentation'><a class='ab-item' href='http://wpdocs.osdn.jp/'>ドキュメンテーション</a></li><li id='wp-admin-bar-support-forums'><a class='ab-item' href='https://ja.wordpress.org/support/'>サポート</a></li><li id='wp-admin-bar-feedback'><a class='ab-item' href='https://ja.wordpress.org/support/forum/feedback/'>フィードバック</a></li></ul></div></li><li id='wp-admin-bar-site-name' class="menupop"><a class='ab-item' aria-haspopup="true" href='http://localhost/wordpress_bs_test/wp-admin/'>X COFFEE</a><div class="ab-sub-wrapper"><ul id='wp-admin-bar-site-name-default' class="ab-submenu"><li id='wp-admin-bar-dashboard'><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-admin/'>ダッシュボード</a></li></ul><ul id='wp-admin-bar-appearance' class="ab-submenu"><li id='wp-admin-bar-themes'><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-admin/themes.php'>テーマ</a></li></ul></div></li><li id='wp-admin-bar-customize' class="hide-if-no-customize"><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-admin/customize.php?url=http%3A%2F%2Flocalhost%2Fwordpress_bs_test%2F'>カスタマイズ</a></li><li id='wp-admin-bar-updates'><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-admin/update-core.php' title='翻訳の更新'><span class="ab-icon"></span><span class="ab-label">1</span><span class="screen-reader-text">翻訳の更新</span></a></li><li id='wp-admin-bar-comments'><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-admin/edit-comments.php'><span class="ab-icon"></span><span class="ab-label awaiting-mod pending-count count-0" aria-hidden="true">0</span><span class="screen-reader-text comments-in-moderation-text">0件のコメントが承認待ちです</span></a></li><li id='wp-admin-bar-new-content' class="menupop"><a class='ab-item' aria-haspopup="true" href='http://localhost/wordpress_bs_test/wp-admin/post-new.php'><span class="ab-icon"></span><span class="ab-label">新規</span></a><div class="ab-sub-wrapper"><ul id='wp-admin-bar-new-content-default' class="ab-submenu"><li id='wp-admin-bar-new-post'><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-admin/post-new.php'>投稿</a></li><li id='wp-admin-bar-new-media'><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-admin/media-new.php'>メディア</a></li><li id='wp-admin-bar-new-page'><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-admin/post-new.php?post_type=page'>固定ページ</a></li><li id='wp-admin-bar-new-user'><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-admin/user-new.php'>ユーザー</a></li></ul></div></li><li id='wp-admin-bar-edit'><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-admin/post.php?post=20&#038;action=edit'>固定ページを編集</a></li></ul><ul id='wp-admin-bar-top-secondary' class="ab-top-secondary ab-top-menu"><li id='wp-admin-bar-search' class="admin-bar-search"><div class="ab-item ab-empty-item" tabindex="-1"><form action="http://localhost/wordpress_bs_test/" method="get" id="adminbarsearch"><input class="adminbar-input" name="s" id="adminbar-search" type="text" value="" maxlength="150" /><label for="adminbar-search" class="screen-reader-text">検索</label><input type="submit" class="adminbar-button" value="検索"/></form></div></li><li id='wp-admin-bar-my-account' class="menupop with-avatar"><a class='ab-item' aria-haspopup="true" href='http://localhost/wordpress_bs_test/wp-admin/profile.php'>こんにちは、<span class="display-name">kijima</span> さん<img alt='' src='http://1.gravatar.com/avatar/4b4c88daa2d4cec56333c7959b08c18d?s=26&#038;d=mm&#038;r=g' srcset='http://1.gravatar.com/avatar/4b4c88daa2d4cec56333c7959b08c18d?s=52&#038;d=mm&#038;r=g 2x' class='avatar avatar-26 photo' height='26' width='26' /></a><div class="ab-sub-wrapper"><ul id='wp-admin-bar-user-actions' class="ab-submenu"><li id='wp-admin-bar-user-info'><a class='ab-item' tabindex="-1" href='http://localhost/wordpress_bs_test/wp-admin/profile.php'><img alt='' src='http://1.gravatar.com/avatar/4b4c88daa2d4cec56333c7959b08c18d?s=64&#038;d=mm&#038;r=g' srcset='http://1.gravatar.com/avatar/4b4c88daa2d4cec56333c7959b08c18d?s=128&#038;d=mm&#038;r=g 2x' class='avatar avatar-64 photo' height='64' width='64' /><span class='display-name'>kijima</span></a></li><li id='wp-admin-bar-edit-profile'><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-admin/profile.php'>プロフィールを編集</a></li><li id='wp-admin-bar-logout'><a class='ab-item' href='http://localhost/wordpress_bs_test/wp-login.php?action=logout&#038;_wpnonce=5cf5b046ef'>ログアウト</a></li></ul></div></li></ul>			</div>
+						<a class="screen-reader-shortcut" href="http://localhost/wordpress_bs_test/wp-login.php?action=logout&#038;_wpnonce=5cf5b046ef">ログアウト</a>
+					</div>
+
+		</body>
+</html>
+<?php echo "index";
