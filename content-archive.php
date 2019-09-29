a911ce2eb6e1413ce126f5964a232eb9a1d3734a
kijima <norimaking777@gmail.com>
2019-09-30 04:07:27 +0900

    複数カテゴリに対応した。。

--- content-archive.php
+++ content-archive.php
@@ -5,10 +5,15 @@
     </a>
   </h2>
   <div class="card-body pt-0">
-    <?php
-    $category = get_the_category();
-    ?>
-    <small class="card-text btn btn-light btn-sm"><?php echo $category[0]->cat_name; ?></small>
+<?php
+    $cats = get_the_category();
+    foreach ($cats as $cat) {
+        ?>
+<small class="card-text btn btn-light btn-sm">
+  <?php echo $cat->cat_name; ?>
+</small>
+<?php
+    }?>
     <small class="card-text"><time pubdate="pubdate" datetime="<?php the_time('Y-m-d'); ?>" class="entry-date">
         <?php the_time(get_option('date_format')); ?></time>
     </small>
--- content.php
+++ content.php
@@ -1,21 +1,29 @@
 <!-- <h2 class="display-4" style="font-weight:900;">
   <?php the_title(); ?>
 </h2> -->
-<small class="card-text btn btn-light btn-sm">タグ1</small>
-<small class="card-text btn btn-light btn-sm">タグ2</small>
+<?php
+    $cats = get_the_category();
+    foreach ($cats as $cat) {
+        ?>
+<small class="card-text btn btn-light btn-sm">
+  <?php echo $cat->cat_name; ?>
+</small>
+<?php
+    }?>
 <small class="card-text"><time pubdate="pubdate" datetime="<?php the_time('Y-m-d'); ?>" class="entry-date">
     <?php the_time(get_option('date_format')); ?></time>
 </small>
-      <a href="<?php the_permalink(); ?>" class="post-link">
-        <?php the_post_thumbnail(
-    'medium',
-    array(
+
+<a href="<?php the_permalink(); ?>" class="post-link">
+  <?php the_post_thumbnail(
+            'medium',
+            array(
 'alt' => the_title_attribute('echo=0'),
 'title' => the_title_attribute('echo=0'),
 'class' => 'card-img',
 )
 ); ?>
-      </a>
+</a>
 <p>
   <?php the_content();?>
   <?php
@@ -41,21 +49,21 @@ endif;
 ?>
 </p>
 
-  <!-- ページ間ナビゲーション -->
-  <?php
+<!-- ページ間ナビゲーション -->
+<?php
 if (is_single()) :
 ?>
-  <div class="container mb-5">
-    <div class="row">
-      <div class="col-md-6 text-left">
-        <?php next_post_link('%link', '%title<i class="fas fa-chevron-left fa-2x float-left text-secondary"></i>', true); ?>
-      </div>
-      <div class="col-md-6 text-right">
-        <?php previous_post_link('%link', '%title<i class="fas fa-chevron-right fa-2x float-right text-secondary"></i>', true); ?>
-      </div>
+<div class="container mb-5">
+  <div class="row">
+    <div class="col-md-6 text-left">
+      <?php next_post_link('%link', '%title<i class="fas fa-chevron-left fa-2x float-left text-secondary"></i>', true); ?>
+    </div>
+    <div class="col-md-6 text-right">
+      <?php previous_post_link('%link', '%title<i class="fas fa-chevron-right fa-2x float-right text-secondary"></i>', true); ?>
     </div>
   </div>
-  <?php
+</div>
+<?php
 endif;
 ?>
-  <!-- ページ間ナビゲーション -->
+<!-- ページ間ナビゲーション -->
