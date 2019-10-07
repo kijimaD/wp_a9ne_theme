54a3b383a36d1b383568b4b2e2d2b5c3bc909aa1
kijima <norimaking777@gmail.com>
2019-10-08 07:11:14 +0900

    紹介サイドバーを作成した。

--- left-sidebar.php
+++ left-sidebar.php
@@ -1,13 +1,17 @@
-<div class="">
-  <div class="card sticky-top mt-2 border-0 float-right" style="width: 10rem;">
-    <img src="<?php bloginfo('template_url'); ?>/images/face.jpg" class="img-thumbnail rounded-circle border-white bg-light ml-3" alt="" style="width: 5rem;">
-    <div class="card-body py-0">
-      <p class="mb-4">
-        貴島 大悟</br>
-        <small>WEB開発者</small>
-      </p>
+<div class="container">
+  <div class="row">
+    <div class="col-6 col-lg-12">
+      <div class="card sticky-top mt-2 border-0 float-right" style="width: 10rem;">
+        <img src="<?php bloginfo('template_url'); ?>/images/face.jpg" class="img-thumbnail rounded-circle border-white bg-light ml-3" alt="" style="width: 5rem;">
+        <div class="card-body py-0">
+          <p class="mb-4">
+            貴島 大悟</br>
+            <small>WEB開発者</small>
+          </p>
+        </div>
+      </div>
     </div>
-    <div class="">
+    <div class="col-6 col-lg-12">
       <p class="" style="line-height:200%;">
         <i class="fas fa-map-marker-alt fa-fw"></i> 鹿児島市<br>
         <a href="https://github.com/kijimaD" class="text-dark"><i class="fab fa-github fa-fw"></i> Github</a><br>
