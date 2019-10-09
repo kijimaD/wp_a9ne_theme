b68754e679e43bc780016d4cc35463650b72570c
kijima <norimaking777@gmail.com>
2019-10-10 00:23:11 +0900

    特定ページでの回り込みを修正した。（カテゴリーループのエラー）

--- content.php
+++ content.php
@@ -3,13 +3,14 @@
   <?php the_title(); ?>
 </h2>
 <!-- /見出し -->
-<?php
+
+<div class="">
+  <ul class="list-inline my-0">
+    <?php
         $cats = get_the_category();
         foreach ($cats as $cat) {
             $catid = $cat->cat_ID;
             $link = get_category_link($catid); ?>
-<div class="">
-  <ul class="list-inline my-0">
     <li class="list-inline-item">
       <a href="<?php echo $link; ?>" class="text-secondary  btn btn-light btn-sm">
         <small class="">
@@ -31,8 +32,8 @@
   </ul>
 </div>
 
-  <a href="<?php the_permalink(); ?>" class="post-link">
-    <?php the_post_thumbnail(
+<a href="<?php the_permalink(); ?>" class="post-link">
+  <?php the_post_thumbnail(
                 'header_image',
                 array(
 'alt' => the_title_attribute('echo=0'),
@@ -40,64 +41,64 @@
 'class' => 'card-img img-thumbnail mb-3',
 )
 ); ?>
-  </a>
+</a>
 
-  <div id="content">
-    <?php the_content();?>
-    <?php
+<div id="content">
+  <?php the_content();?>
+  <?php
   if (is_single()) :
   ?>
-    <hr>
-    <div id="content_date_author">
-      <ul class="list-inline text-right my-0">
-        <li class="list-inline-item">
-          <small>
-            <?php get_template_part('sns'); ?>
-          </small>
-        </li>        
-        <li class="list-inline-item">
-          <time pubdate="pubdate" datetime"<?php the_time('Y-m-d'); ?>" class="entry-date">
-            <small class="text-muted">
-              <?php the_time(get_option('date_format')); ?></small>
-          </time>
-        </li>
-        <li class="list-inline-item">
+  <hr>
+  <div id="content_date_author">
+    <ul class="list-inline text-right my-0">
+      <li class="list-inline-item">
+        <small>
+          <?php get_template_part('sns'); ?>
+        </small>
+      </li>
+      <li class="list-inline-item">
+        <time pubdate="pubdate" datetime"<?php the_time('Y-m-d'); ?>" class="entry-date">
           <small class="text-muted">
-            <?php the_author_posts_link(); ?></small>
-        </li>
-      </ul>
-    </div>
-    <?php
+            <?php the_time(get_option('date_format')); ?></small>
+        </time>
+      </li>
+      <li class="list-inline-item">
+        <small class="text-muted">
+          <?php the_author_posts_link(); ?></small>
+      </li>
+    </ul>
+  </div>
+  <?php
 endif;
 ?>
-  </div>
+</div>
 
-  <!-- ページ間ナビゲーション -->
-  <?php
+<!-- ページ間ナビゲーション -->
+<?php
 if (is_single()) :
 ?>
-  <div class="container mb-5">
-    <div class="row">
-      <div class="col-md-6 text-left mx-0 pr-1">
-        <small>＜ 次の記事</small>
-        <?php next_post_link('%link', '%title', false); ?>
-      </div>
-      <div class="col-md-6 text-right mx-0 pl-1">
-        <small>前の記事 ＞</small>
-        <?php previous_post_link('%link', '%title', false); ?>
-      </div>
+<div class="container mb-5">
+  <div class="row">
+    <div class="col-md-6 text-left mx-0 pr-1">
+      <small>＜ 次の記事</small>
+      <?php next_post_link('%link', '%title', false); ?>
+    </div>
+    <div class="col-md-6 text-right mx-0 pl-1">
+      <small>前の記事 ＞</small>
+      <?php previous_post_link('%link', '%title', false); ?>
     </div>
   </div>
-  <?php
+</div>
+<?php
 endif;
 ?>
-  <!-- ページ間ナビゲーション -->
+<!-- ページ間ナビゲーション -->
 
-  <!-- スマホ時の自己紹介 -->
-  <!-- <div class="d-block d-lg-none">
+<!-- スマホ時の自己紹介 -->
+<!-- <div class="d-block d-lg-none">
   <?php
 get_template_part('left-sidebar');
 ?>
 </div> -->
 
-  <!-- /スマホ時の自己紹介 -->
+<!-- /スマホ時の自己紹介 -->
