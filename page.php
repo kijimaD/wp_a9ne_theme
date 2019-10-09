c28f4eb7c62e50cf033bcdeceabe56dedcbdc026
kijima <norimaking777@gmail.com>
2019-10-09 23:31:56 +0900

    固定ページと記事ページを同期した（同じテンプレートでよくない）。

--- page.php
+++ page.php
@@ -1,18 +1,12 @@
 <?php get_header(); ?>
 
-<!-- 左サイドバー -->
-<div class="col-lg-2  d-none d-lg-block">
-<?php get_template_part('left-sidebar'); ?>
-</div>
-<!-- /左サイドバー -->
-
 <!-- 空間 -->
-<div class="col-lg-1">
+<div class="col-lg-1 order-lg-2">
 </div>
 <!-- /空間 -->
 
 <!-- 本文 -->
-<div class="col-lg-7">
+<div class="col-lg-7 order-lg-3">
   <?php
 if (have_posts()) :
   while (have_posts()):
@@ -28,11 +22,16 @@ if (have_posts()) :
   <!-- /ペジネーション -->
 
 </div>
-
 <!-- /本文 -->
 
+<!-- 左サイドバー -->
+<div class="col-lg-2 order-lg-1">
+  <?php get_template_part('left-sidebar'); ?>
+</div>
+<!-- /左サイドバー -->
+
 <!-- 未定・右 -->
-<div class="col-lg-3">
+<div class="col-lg-2 order-lg-4">
 </div>
 <!-- /未定・右 -->
 
