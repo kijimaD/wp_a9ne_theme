110098bcecdd3c755dc5c34c7ca054aa2da3a540
kijima <norimaking777@gmail.com>
2019-10-10 09:10:44 +0900

    フッターを調整した。

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
-<footer class="py-4">
+<footer class="py-4 bg-light">
   <div class="container text-center">
-    <hr>
     <small>
       Copyright &copy; 2019
       <?php bloginfo('name'); ?>, All Rights Reserved.
